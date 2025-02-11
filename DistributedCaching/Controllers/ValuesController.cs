e9185e3ee35ddff6611d3efcff1639152235b399
buzlukerdem <buzlukerdem@gmail.com>
2025-02-11 17:44:52 +0300

    Add project files.

---
+++ DistributedCaching/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace DistributedCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet("set/{name}/{surname}")]
+
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            
+            await _distributedCache.SetStringAsync("name", name, options: new()
+            {
+                //config
+                AbsoluteExpiration = DateTime.Now.AddMinutes(30),
+                SlidingExpiration = TimeSpan.FromMinutes(10)
+            });
+            await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname), options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(30),
+                SlidingExpiration = TimeSpan.FromMinutes(10)
+            });
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+            var name = await _distributedCache.GetStringAsync("name");
+            var surnameB = await _distributedCache.GetAsync("surname");
+            var surname = Encoding.UTF8.GetString(surnameB);
+            return Ok(new
+            {
+                name,
+                surname
+            });
+        }
+    }
+}
---
+++ InMemoryCaching/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        // Basic Example
+        [HttpGet("set/{name}")]
+        public void Set(string name)
+        {
+            // Key-Value format
+            _memoryCache.Set("name", name);
+            Console.WriteLine("başarılı");
+
+        }
+        [HttpGet("get")]
+        public string Get()
+        {
+            //return _memoryCache.Get<string>("name");
+            if (_memoryCache.TryGetValue<string>("name", out string name))
+            {
+                return name.Substring(5);
+            }
+            return "veri set edilmedi";
+        }
+
+
+
+        // absolute - sliding exapmle
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                // Life Time: 30 seconds
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                // Must be get in 5 seconds
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+
+    }
+}
